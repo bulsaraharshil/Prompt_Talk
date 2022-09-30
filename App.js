7d6a66e5fa5cf3abd72f6797ab71b7572a969874
Dhruvi Champaneriya <dhruvichampaneriya22@gmail.com>
2022-09-29 21:27:23 -0400

    style: changed color for testing

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a0a0a0',
     alignItems: 'center',
     justifyContent: 'center',
   },
