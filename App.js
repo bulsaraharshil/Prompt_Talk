b9ba98acdae49e76ec85b63001b96ee01db5a819
Harshil Bulsara <bulsaraharshil@gmail.com>
2022-10-08 22:38:37 -0400

    style: created basic home page of application

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.joinText}>Let's Connect</Text>
+      <TextInput style={styles.nameInput} placeholder="Enter Name" placeholderTextColor="white"/>
+      <TextInput style={styles.roomInput} placeholder="Enter Room" placeholderTextColor="white"/>
+      <Button title="Join" onPress={() => alert('Joining...')}/>
     </View>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Prompt Talk" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#a0a0a0',
+    backgroundColor: '#1c1c1c', 
     alignItems: 'center',
     justifyContent: 'center',
   },
+  joinText:{
+    color:"white",
+    fontSize: 35
+  },
+  nameInput:{
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    color:"white",
+    padding:12,
+    margin:5
+  },
+  roomInput:{
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    color:"white",
+    padding:12,
+    margin:5
+  }
 });
+
+
+export default App;
