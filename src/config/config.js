31c6fc83e3ebd034285b87419201ad6fa7321da8
Harshil Bulsara <bulsaraharshil@gmail.com>
2022-11-10 11:52:37 -0500

    changes in config

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,8 @@
+import { Platform } from 'react-native';
 import io from 'socket.io-client';
 
-export const API_URL = '"http://127.0.0.1:3000"';
+// export const API_URL = 'http://127.0.0.1:3000';
+
+export const API_URL = Platform.OS === 'ios' ? 'http://192.168.99.213:3000' : 'http://192.168.99.213:3000';
+
 export const SOCKET = io(API_URL);
