27e307e99d11cc72e20bc855f7c1c055f435b570
Harshil Bulsara <bulsaraharshil@gmail.com>
2022-11-14 11:14:36 -0500

    changes in backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 import { Platform } from 'react-native';
 import io from 'socket.io-client';
 
-// export const API_URL = 'http://127.0.0.1:3000';
+export const API_URL = 'http://10.0.0.99:3000'; //this is the IP address of my computer and it changes when I connect to a different network
 
-export const API_URL = Platform.OS === 'ios' ? 'http://192.168.99.213:3000' : 'http://192.168.99.213:3000';
+// export const API_URL = Platform.OS === 'ios' ? 'http://192.168.99.213:3000' : 'http://192.168.99.213:3000';
 
 export const SOCKET = io(API_URL);
